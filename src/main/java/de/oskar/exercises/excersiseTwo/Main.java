090d9f8fcd43d5207fb8a82f336d7aab06be2ad2
Mortis <oskar.sammet@gmail.com>
2024-03-25 11:26:11 +0100

    save

--- src/main/java/de/oskar/exercises/excersiseTwo/Main.java
+++ src/main/java/de/oskar/exercises/excersiseTwo/Main.java
@@ -1,13 +1,5 @@
 package main.java.de.oskar.exercises.excersiseTwo;
 
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-
 public class Main {
-    public static void main(String[] args) {
-        System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-        Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
-        System.out.println("mat=" + mat.dump());
-    }
+    public static void main(String[] args) {}
 }
